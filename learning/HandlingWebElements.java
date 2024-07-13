25b00ac87bd89d13883255ca90674c04b6a1bd93
Kajal Pawar <kajalpawar2895@gmail.com>
2024-07-13 11:10:21 +0530

    Modified HandlingWebElements.java

--- learning/HandlingWebElements.java
+++ learning/HandlingWebElements.java
@@ -21,7 +21,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 
-public class HandlingWebElementsexamplesFeatureSchedule {
+public class HandlingWebElements {
 	WebDriver driver;
 //This line added n mian branch
 //	@Test
@@ -37,7 +37,7 @@ public class HandlingWebElementsexamplesFeatureSchedule {
 			airbags.click();
 		}
 		
-		Thread.sleep(3000);
+		Thread.sleep(300);
 		//Uncheck the checkbox
 		if (airbags.isSelected()) {
 			airbags.click();
@@ -61,7 +61,7 @@ public class HandlingWebElementsexamplesFeatureSchedule {
 		List<WebElement> allOptions = make.findElements(By.tagName("option"));
 		System.out.println("Total options in the dropdown: " + allOptions.size());
 	
-		Thread.sleep(3000);
+		Thread.sleep(7000);
 		
 		
 		Select makeSelect = new Select(make);
